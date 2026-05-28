420eb6ed0ba7eec36641d59231453e31ab7cbc9d
suzuuuuuu7 <sujalkc324@gmail.com>
2026-05-28 14:52:41 +0545

    updated

--- learn_react/src/App.jsx
+++ learn_react/src/App.jsx
@@ -1,7 +1,17 @@
 import Sujal from "./sujal";
 function App() {
+  let username = "Sujal K.C."
   return (
-  <Sujal/>
+    <>  <Sujal/>
+    <h1>{username}</h1>
+    <p> You can call me sujal</p>
+    <button> Submit</button>
+    <div></div>
+    <div></div>
+    </>
+ 
+  
+
   );
 }
 
--- learn_react/src/main.jsx
+++ learn_react/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+//  const Reactelement = {  // This is how react treat our html code
+//     type:"a",
+//     props:{
+//         href:"https/google.com",
+//         target:"_blank"
+//     },
+//     Children:"Click me here",
+//  };
+const reactelement =React.createElement(
+    'a',
+    {href: "https://google.com",target:"_blank"},
+    "click me access youtube"
+)
+function Myapp(){
+    return(
+        <div> Custome app</div>
+    )
+}
+const anotherelement = (
+    <a href = "https://youtube.com" target = "_blank">open link</a>
+
+)
+const myelement = "your name"
+
+    
 
 createRoot(document.getElementById('root')).render(
  
-    <App />
+<Myapp/>
 
 )
